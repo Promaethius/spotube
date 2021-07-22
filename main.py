d03637a1883e53edc785115d6d237d8619f47ccc
Promaethius <bryant.jonathan.42@gmail.com>
2021-07-22 12:23:12 -0600

    async magic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ytmusicapi import YTMusic
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import html, click
+import concurrent.futures as cf
 
 @click.command()
 @click.option('--ytheaders', priompt='Paste YouTube Music request headers:',
@@ -11,6 +12,7 @@ import html, click
 @click.option('--spotify', default=True, help='Set to False to disable syncing to Spotify. Default: True')
 @click.option('--ytmusic', default=True, help='Set to False to disable syncing to YouTube Music. Default: True')
 @click.option('--sync_target', default='albums', help='Targets item types to sync. Currently supported are albums, songs, and playlists. Multiple instances of this command are allowed.')
+@click.option('--threads', default=5, help='Advanced Command. Specifies the number of sub-threads to use when fetching items and syncing.')
 
 class Album:
     def __init__(self, item, platform) -> None:
@@ -34,8 +36,43 @@ class Album:
         self.setPlatform(platform)
         pass
 
-    def getType(self):
-        return self._type
+    def getTitle(self):
+        return self._title
+    def getArtists(self):
+        return self._artists
+    def getPlatforms(self):
+        return self._platform
+    def setPlatform(self, platform):
+        if platform == "ytmusic":
+            self._platform['ytmusic'] = True
+        elif platform == "spotify":
+            self._platform['spotify'] = True
+
+class Song:
+    def __init__(self, item, platform) -> None:
+        self._title = ""
+        self._artists = []
+        self._album = ""
+        self._platform = {
+            'ytmusic': False,
+            'spotify': False
+        }
+        
+        if 'name' in item.keys():
+            self._title = item['name']
+        elif 'title' in item.keys():
+            self._title = item['title']
+
+        for artist in item['artists']:
+            self._artists.append(
+                artist['name']
+            )
+        
+        self._album = item['album']
+        
+        self.setPlatform(platform)
+        pass
+
     def getTitle(self):
         return self._title
     def getArtists(self):
@@ -103,24 +140,26 @@ class Spotube:
                     self._sp.current_user_saved_albums_add([album['id']])
                     return
 
-    def sync(self, spotify, ytmusic, sync_target):
-        for album in self._paginationHelper(self._sp.current_user_saved_albums()):
-            self._processAlbum(album['album'], "spotify")
-
-        for album in self._ytm.get_library_albums(limit=1000):
-            self._processAlbum(album, "ytmusic")
-
-        for _item in self._items:
+    def sync(self, spotify, ytmusic, sync_target, threads):
+        with cf.ThreadPoolExecutor(max_workers=threads) as executor:
             if 'albums' in sync_target:
-                if not _item.getPlatforms()['ytmusic'] and spotify:
-                    self._ytmAddAlbum(_item.getTitle(), _item.getArtists())
-                elif not _item.getPlatforms()['spotify'] and ytmusic:
-                    self._spotifyAddAlbum(_item.getTitle(), _item.getArtists())
+                for album in self._paginationHelper(self._sp.current_user_saved_albums()):
+                    executor.submit(self._processAlbum, album['album'], "spotify")
+                for album in self._ytm.get_library_albums(limit=1000):
+                    executor.submit(self._processAlbum, album, "ytmusic")
+
+        with cf.ThreadPoolExecutor(max_workers=threads) as executor:
+            for _item in self._items:
+                if isinstance(_item, Album) and 'albums' in sync_target:
+                    if not _item.getPlatforms()['ytmusic'] and spotify:
+                        executor.submit(self._ytmAddAlbum, _item.getTitle(), _item.getArtists())
+                    elif not _item.getPlatforms()['spotify'] and ytmusic:
+                        executor.submit(self._spotifyAddAlbum, _item.getTitle(), _item.getArtists())
 
 
-def main(ytheaders, spotify_client_id, spotify_client_secret, spotify, ytmusic, sync_target):
+def main(ytheaders, spotify_client_id, spotify_client_secret, spotify, ytmusic, sync_target, threads):
     spotube = Spotube(ytheaders, spotify_client_id, spotify_client_secret)
-    spotube.sync(spotify, ytmusic, sync_target)
+    spotube.sync(spotify, ytmusic, sync_target, threads)
     quit()
 
 if __name__ == "__main__":
